e691a4bd43cceb2aaea793eb38e677036542edf8
adembenarfa175-code <adembenarfa175@gmail.com>
2025-12-27 20:13:27 +0100

    Release v2025.6.3: Optimized Memory & Architecture Sync

--- arch/x86/mbr/boot.asm
+++ arch/x86/mbr/boot.asm
@@ -2,7 +2,7 @@
 [org 0x7c00]
 jmp short start
 nop
-db "ZRBL6.3 " ; OEM ID
+db "ZRBL6.3 "
 start:
     cli
     xor ax, ax
@@ -13,8 +13,15 @@ start:
     mov si, stage2_msg
     call print
     jmp 0x0800:0000
-print: lodsb | or al, al | jz .d | mov ah, 0x0e | int 0x10 | jmp print
-.d: ret
+print:
+    lodsb
+    or al, al
+    jz .d
+    mov ah, 0x0e
+    int 0x10
+    jmp print
+.d:
+    ret
 stage2_msg db "ZRBL v2025.6.3 x86 MBR OK", 0
 times 510-($-$$) db 0
 dw 0xaa55
--- kernel/zrbl_kernel.c
+++ kernel/zrbl_kernel.c
@@ -1,7 +1,6 @@
 #include "../common/zrbl_common.h"
 void zrbl_kernel_main() {
     zrbl_log("Kernel Initialized v2025.6.3");
-    // Memory fix: verify stack alignment
-    asm volatile("and $0xfffffff0, %esp");
+    __asm__ volatile("and $0xfffffff0, %%esp" : : : "esp");
     while(1);
 }
